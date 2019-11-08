6ec9cd4f47b34f91edbe5bc3e014cc4ffc2d3a15
alireza <sarsanaee@gmail.com>
2019-11-08 12:05:13 -0700

    two run script for the send and receive applications

---
+++ exp/exp_run_rx.py
@@ -0,0 +1,226 @@
+#!/usr/bin/python
+
+import argparse
+import csv
+import glob
+import json
+import math
+import os
+import platform
+import re
+import shlex
+import subprocess
+import sys
+import json
+
+from time import sleep
+
+BESS_CONFIG_PATH = '/proj/uic-dcs-PG0/post_loom/one_to_one_docker.bess'
+
+BESS_HOME = '/proj/uic-dcs-PG0/post-loom/code/bess'
+
+MOON_HOME = '/proj/uic-dsc-PG0/'
+
+class VhostConf(object):
+    def __init__(self, *initial_data, **kwargs):
+        for dictionary in initial_data:
+            for key in dictionary: 
+                setattr(self, key, dictionary[key]) 
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+
+def bess(config):
+    param_path = "config/exp_config.json"
+    config_path = "/proj/uic-dcs-PG0/post-loom/code/bess/bessctl/conf/port/dcfc_sw.bess"
+    cmd = 'sudo bessctl/bessctl -- daemon start -- run file {config_file}'.format(config_file=os.path.join(BESS_HOME, 
+        "bessctl/conf/port", config['bess_config_path']))
+    print cmd
+    bessctl_proc = subprocess.check_call(cmd, cwd=BESS_HOME, shell=True)
+
+def make_sink_app(dpdk_home):
+    cmd = 'make -C examples/skeleton RTE_SDK=$(pwd) RTE_TARGET=build O=$(pwd)/build/examples/skeleton'
+    make_proc = subprocess.check_call(cmd, cwd=dpdk_home, shell=True)
+
+def sink_app(just_compile):
+    DPDK_HOME='/proj/uic-dcs-PG0/dpdk-stable-17.11.6/'
+    make_sink_app(DPDK_HOME)
+    cmd_fw_sink = 'sudo ./build/examples/skeleton/basicfwd -l 0-1 -n 4 --vdev="virtio_user0,path=/users/alireza/my_vhost0.sock,queues=1" --log-level=8 --socket-mem 1024,1024 --proc-type auto'
+    if(not just_compile):
+        subprocess.check_call(cmd_fw_sink, cwd=DPDK_HOME, shell=True)
+
+def moongen_run(config):
+    MOON_HOME="/proj/uic-dcs-PG0/moongen/"
+    cmd = 'sudo ./build/MoonGen examples/{script_to_load} --dpdk-config={dpdk_conf} {sender_dev} {receiver_dev} {sender} {receiver} --edrop {enable_drop} -r {main_workload_rate} -t {exp_duration}'.format(
+            sender_dev = config['sender_dev'],
+            receiver_dev = config['receiver_dev'],
+            main_workload_rate = config['main_workload_rate'],
+            exp_duration = config['exp_duration'],
+            script_to_load = config['script'],
+            enable_drop = config['drop'],
+            sender = config['is_sender'],
+            receiver = config['is_recv'],
+            dpdk_conf = config['dpdk_config']
+            )
+
+    #cmd = 'sudo ./build/MoonGen examples/{script_to_load} --dpdk-config={dpdk_conf} {sender_dev} {receiver_dev} --bpressure {backpressure} --edrop {enable_drop} -r {main_workload_rate} --brate {background_workload_rate} -t {exp_duration}'.format(
+    #        sender_dev = config['sender_dev'],
+    #        receiver_dev = config['receiver_dev'],
+    #        main_workload_rate = config['main_workload_rate'],
+    #        background_workload_rate = config['background_workload_rate'],
+    #        exp_duration = config['exp_duration'],
+    #        script_to_load = config['script'],
+    #        enable_drop = config['drop'],
+    #        backpressure = config['backpressure'],
+    #        dpdk_conf = config['dpdk_config']
+    #        )
+    print(cmd)
+    moon_gen_proc = subprocess.check_call(cmd, cwd=MOON_HOME, shell=True)
+    print("Moongen exits")
+    #moongen_post_exp()
+
+def moongen_run_exp(config):
+    MOON_HOME="/proj/uic-dcs-PG0/moongen/"
+    cmd = 'sudo ./build/MoonGen examples/{script_to_load} --dpdk-config={dpdk_conf} {sender_dev} {receiver_dev} --bpressure {backpressure} --edrop {enable_drop} -r {main_workload_rate} --brate {background_workload_rate} -t {exp_duration}'.format(
+            sender_dev = config['sender_dev'],
+            receiver_dev = config['receiver_dev'],
+            main_workload_rate = config['main_workload_rate'],
+            background_workload_rate = config['background_workload_rate'],
+            exp_duration = config['exp_duration'],
+            script_to_load = config['script'],
+            enable_drop = config['drop'],
+            backpressure = config['backpressure'],
+            dpdk_conf = config['dpdk_config']
+            )
+
+    print(cmd)
+    moon_gen_proc = subprocess.check_call(cmd, cwd=MOON_HOME, shell=True)
+    print("Moongen exits")
+    moongen_post_exp(config)
+
+def moongen_post_exp(config):
+    results_dir = "/proj/uic-dcs-PG0/post-loom/exp/results"
+    cmd = "count=`ls -1 pings* | wc -l` && mv pings.txt pings_${count}.txt"
+    subprocess.check_call(cmd, cwd=results_dir, shell=True)
+    analysis(results_dir, config)
+
+def analysis(results_dir, config):
+    analysis_dir = "/proj/uic-dcs-PG0/post-loom/exp/analysis"
+    cmd = "count=`ls -1  {0}/pings* | wc -l` && python3 analysis.py {0} {0}/pings_$count.txt {1} {2}".format(results_dir, config['background_workload_rate'], config['drop'])
+    subprocess.check_call(cmd, cwd=analysis_dir, shell=True)
+
+def analysis_manual(results_dir):
+    analysis_dir = "/proj/uic-dcs-PG0/post-loom/exp/analysis"
+    pings = results_dir + "/" + "pings_3.txt"
+    cmd = "python3 analysis.py {0} {1}".format(results_dir, pings)
+    subprocess.check_call(cmd, cwd=analysis_dir, shell=True)
+
+def load_exp_conf(config_path):
+    with open(config_path) as config_file:
+        data = json.load(config_file)
+    return data
+
+
+def drop_latency_experiment():
+    #bess_config(BESS_CONFIG_PATH) #TODO: I need to involve the config from here
+ 
+    config = VhostConf(load_exp_conf("config/exp_config.json"))
+    plot_data_cleanup(config.plot)
+    
+    for j in range(2):
+        config.moongen['drop'] = j
+        i = 0.5 
+        while(i < 7):
+            config.moongen['background_workload_rate'] = i
+            moongen_run_exp(config.moongen)
+            i = i + 0.5
+    
+    draw_latency_drop_plots(config.plot)
+
+def plot_data_cleanup(config):
+    PLOT_HOME = config['plot_dir']
+    drawer = config['drawer']
+    cmd = "rm {0} {1} {2} {3} {4}".format(config['latency_0'],
+            config['drop_0'], config['latency_1'],
+            config['drop_1'], config['background_rate'])
+    subprocess.call(cmd, cwd=PLOT_HOME, shell=True)
+
+def draw_latency_drop_plots(config):
+    PLOT_HOME = config['plot_dir']
+    drawer = config['drawer']
+    cmd = 'python3 {0} {1} {2} {3} {4} {5}'.format(config['drawer'], config['latency_0'],
+            config['drop_0'], config['latency_1'],
+            config['drop_1'], config['background_rate'])
+    subprocess.check_call(cmd, cwd=PLOT_HOME, shell=True)
+
+def redraw_latency_drop_plot():
+    config = VhostConf(load_exp_conf("config/exp_config.json"))
+    draw_latency_drop_plots(config.plot)
+
+def sysbench(config):
+    #cmd = "taskset --cpu-list 12,14,16 sysbench --test=cpu --cpu-max-prime=200000000 --num-threads=%s run &" %config["thread_number"]
+    cmd = "taskset --cpu-list 16 sysbench --test=cpu --cpu-max-prime=200000000 --num-threads=%s run &" %config["thread_number"]
+    subprocess.call(cmd, shell=True)
+
+def kill_sysbench():
+    cmd = "pkill sysbench"
+    subprocess.check_call(cmd, shell=True)
+
+def motive1(config_path):
+    config = VhostConf(load_exp_conf(config_path))
+    general_config = VhostConf(config.general)
+    res_path = '/proj/uic-dcs-PG0/post-loom/exp/results'
+
+    if(general_config.sysbench):
+        sysbench(config.sysbench)
+
+    if(general_config.bess):
+        bess(config.bess)
+
+    if(general_config.moongen):
+        moongen_run(config.moongen)
+
+    if(general_config.sysbench):
+        kill_sysbench()
+
+
+    if(config.moongen['drop']):
+        res_path = os.path.join(res_path, 'Backpressure')
+    else:
+        res_path = os.path.join(res_path, 'PFC')
+
+    if(general_config.sysbench):
+        res_path = os.path.join(res_path, 'non-isolated')
+    else:
+        res_path = os.path.join(res_path, 'isolated')
+
+    if config.moongen['main_workload_rate'] < 5:
+        res_path = os.path.join(res_path, str(config.moongen['main_workload_rate']*1000))
+    else:
+        res_path = os.path.join(res_path, str(config.moongen['main_workload_rate']))
+
+
+    
+    cmd = 'mkdir -p %s' %res_path
+    subprocess.check_call(cmd, shell=True)
+    cmd = 'yes | mv results/42_rx.csv results/FG_tx.csv results/pings.txt %s' %res_path
+    subprocess.check_call(cmd, shell=True)
+
+
+
+
+#os.environ["RTE_SDK"] = "/proj/uic-dcs-PG0/post-loom/code/dpdk/"
+#os.environ["RTE_TARGET"] = "x86_64-native-linuxapp-gcc"
+config = VhostConf(load_exp_conf('config/rx.json'))
+
+
+#motive1("config/motive_config_1.json")
+#motive1("config/motive_config_2.json")
+#motive1("config/motive_config_3.json")
+#motive1("config/motive_config_4.json")
+#run_sysbench()
+#bess_config("config/motive_config.json")
+bess(config.bess)
+moongen_run(config.moongen)
+#analysis_manual("/proj/uic-dcs-PG0/post-loom/exp/results/")
+#drop_latency_experiment()
+#redraw_latency_drop_plot()
