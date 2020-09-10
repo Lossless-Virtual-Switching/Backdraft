bf1bcd257661aa96364654f3a3ac1825995a9b94
Alireza <sarsanaee@gmail.com>
2020-09-10 09:18:27 -0500

    Fixing interface name in the setup scripts

--- exp/short_long_flow/cluster_config_parse_utils.py
+++ exp/short_long_flow/cluster_config_parse_utils.py
@@ -7,7 +7,11 @@ def get_mac_address():
     """
     Get current device MAC address
     """
-    res = subprocess.run('cat /sys/class/net/eno50/address', shell=True, stdout=subprocess.PIPE)
+    import json
+
+    config = json.load(open(".pipeline_config.json", "r"))
+
+    res = subprocess.run('cat /sys/class/net/{}/address'.format(config["interface"]), shell=True, stdout=subprocess.PIPE)
     add = res.stdout.decode().strip()
     return add
 
@@ -23,16 +27,23 @@ def get_pci_address(iface):
 def get_current_node_info(file_path):
     mac = get_mac_address()
     found = False
+    t_key = None
+    t_value = None
     with open(file_path) as f:
       config = yaml.load(f, Loader=yaml.FullLoader)
       for key in config.keys():
         item = config[key]
-        if item['mac'] == mac:
+        if item['mac'].strip() == mac.strip():
+          t_key = key
+          t_value = config[key]
           found = True
           break
+
       if not found:
         return (None, None)
-    return (key, item)
+
+    print(t_key, t_value)
+    return (t_key, t_value)
 
 
 def get_full_config(file_path):
