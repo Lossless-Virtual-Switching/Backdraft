0b08eb7f0677fd57fa12421231ca7753d10aac2e
Alireza Sanaee <sarsanaee@gmail.com>
2021-08-18 12:47:46 -0600

    creating new scripts for homa experiments

---
+++ exp/homa_bkdrft_multi_node/client_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo pkill bessd
+sudo pkill dpdk_test
+sudo pkill dpdk_client_system_test
+./run_exp.py 1 3 client --drop="0.0" --vswitch_path ../../../homa-bess/bess/ --pci 41:00.0 --count_queue 3 --queue_size 256 --time 10 --tx_size 1000
