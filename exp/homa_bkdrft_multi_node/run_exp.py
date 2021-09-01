0d4d1430ed150e4bb8ef19d7bf810e81402734d8
Alireza Sanaee <sarsanaee@gmail.com>
2021-09-01 02:57:25 -0600

    adding pfq solution

--- exp/homa_bkdrft_multi_node/run_exp.py
+++ exp/homa_bkdrft_multi_node/run_exp.py
@@ -21,6 +21,7 @@ pipeline_config_file = os.path.join(cur_script_dir,
     # '../homa/pipeline.bess')
     # 'pipeline_bd.bess')
     'pipeline_incast_bd.bess')
+    # 'pipeline_incast_pfq.bess')
 
 homa_base = os.path.join(cur_script_dir, '../../code/Homa')
 homa_app_bin = os.path.join(homa_base, 'build/test/dpdk_test')
@@ -133,6 +134,11 @@ def main():
     server_process = []
     client_process = []
 
+    ret = bessctl_do('show pipeline', subprocess.PIPE)
+    print(ret.stdout.decode().strip())
+
+    # exit(0)
+
     if mode == "server": 
         # run_server(server_conf)
         for i in range(vhost_port_count):
@@ -221,9 +227,9 @@ def main():
       sum_pkts += pkts
       sum_bytes += byte
 
-    # ret = bessctl_do('show tc', subprocess.PIPE)
-    # log = ret.stdout.decode()
-    # print(log)
+    ret = bessctl_do('show tc', subprocess.PIPE)
+    log = ret.stdout.decode()
+    print(log)
 
     for i in range(vhost_port_count * 2):
       # pause frame
