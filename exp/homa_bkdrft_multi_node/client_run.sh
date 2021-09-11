43c80a6917d63c5056ed2981c14dbbeb6e191a28
Alireza Sanaee <sarsanaee@gmail.com>
2021-09-11 04:40:48 -0600

    three node experiment for Homa

--- exp/homa_bkdrft_multi_node/client_run.sh
+++ exp/homa_bkdrft_multi_node/client_run.sh
@@ -5,10 +5,10 @@ sudo pkill dpdk_client_system_test
 sudo rm /dev/shm/dpdk_test6 &> /dev/null
 sudo rm /dev/shm/homa_openloop_dpdk_test &> /dev/null
 
-# ./run_exp.py 1 2 client --drop="0.0" --vswitch_path ../../../homa-bess/bess/ \
-# 	--pci 03:00.1 --count_queue 3 --queue_size 256 --time 10 \
-# 	--tx_size 200 --delay 10000
+ ./run_exp.py 1 3 client --drop="0.0" --vswitch_path ../../../homa-bess/bess/ \
+ 	--pci 41:00.0 --count_queue 4 --queue_size 256 --time 10 \
+  	--tx_size 200 --delay 10000
 
-./run_exp.py 1 2 client --drop="0.0" --vswitch_path ../../../homa-bess/bess/ \
-	--pci 03:00.1 --count_queue 1 --queue_size 256 --time 10 \
-	--tx_size 200 --delay 5000
+# ./run_exp.py 1 3 client --drop="0.0" --vswitch_path ../../../homa-bess/bess/ \
+# 	--pci 41:00.0 --count_queue 1 --queue_size 256 --time 10 \
+# 	--tx_size 200 --delay 5000
