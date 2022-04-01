01f7119955c082f6f0bd4fe6d3732f4bb161db21
Alireza Sanaee <sarsanaee@gmail.com>
2022-04-01 09:51:10 -0500

    Delete exp/analysis directory

--- exp/analysis/drop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in `seq 100 -2 0`; do
-	filename="/home/alireza/Documents/post-loom/exp/results/latency_0_"
-	filename="/proj/uic-dcs-PG0/post-loom/exp/results/latency_0_"
-	filename=$filename$i
-	cat $filename".txt" | grep "packets" | awk '{print($10)}'
-	#echo $i
-done
